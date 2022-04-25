67fece499a8fa6d608c204b19c3ca3521c8719ef
Nik Zandian <nik@quietrocket.com>
2022-04-25 17:44:20 +0200

    Rendering works properly

--- scripts/Labyrinth.gd
+++ scripts/Labyrinth.gd
@@ -1,14 +1,17 @@
 extends GridMap
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_maze("res://mazes/example1.txt")
+
+func load_maze(path: String):
 	var file = File.new()
-	file.open("res://mazes/simple.txt", File.READ)
+	file.open(path, File.READ)
 
 	while !file.eof_reached():
 		var line = file.get_csv_line(" ")
 		if line.size() == 6:
+			# Format: N E S W
 			var directions = {
 				"north": line[2].to_int(),
 				"east": line[3].to_int(),
@@ -20,14 +23,18 @@ func _ready():
 	file.close()
 
 const SCALE = 5
-const XOFF = 10
-const YOFF = 10
+const XOFF = -2
+const YOFF = 0
 
 func process_line(x, y, directions):
-	var ax = x * SCALE + XOFF
-	var ay = y * SCALE + YOFF
+	var ax = x * (SCALE - 1) + XOFF
+	var ay = y * (SCALE - 1) + YOFF
 	
+	# Fill in the corners
 	set_block(ax, ay)
+	set_block(ax + 4, ay)
+	set_block(ax, ay - 4)
+	set_block(ax + 4, ay - 4)
 	
 	if directions["north"] == 1:
 		set_block(ax + 1, ay)
@@ -35,19 +42,19 @@ func process_line(x, y, directions):
 		set_block(ax + 3, ay)
 	
 	if directions["south"] == 1:
-		set_block(ax + 1, ay + 4)
-		set_block(ax + 2, ay + 4)
-		set_block(ax + 3, ay + 4)
+		set_block(ax + 1, ay - 4)
+		set_block(ax + 2, ay - 4)
+		set_block(ax + 3, ay - 4)
 		
 	if directions["east"] == 1:
-		set_block(ax + 4, ay + 1)
-		set_block(ax + 4, ay + 2)
-		set_block(ax + 4, ay + 3)
+		set_block(ax + 4, ay - 1)
+		set_block(ax + 4, ay - 2)
+		set_block(ax + 4, ay - 3)
 	
 	if directions["west"] == 1:
-		set_block(ax, ay + 1)
-		set_block(ax, ay + 2)
-		set_block(ax, ay + 3)
+		set_block(ax, ay - 1)
+		set_block(ax, ay - 2)
+		set_block(ax, ay - 3)
 
 func set_block(x: int, y: int):
 	self.set_cell_item(Vector3i(x, 0, -y), 0, 0)
